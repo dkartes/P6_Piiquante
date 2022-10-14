658e0de33ae5f0f581423582b593a486a6343940
dkartes <hetyei.kevin@gmail.com>
2022-10-14 16:56:58 +0200

    Ajout des models + routes + controllers

--- Back/server.js
+++ Back/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/user.routes");
 // on appel notre .env qui contient nos variables d'environnement
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+app.use("/api/auth", userRoutes);
+
+//server
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
