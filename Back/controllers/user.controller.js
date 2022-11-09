3d4b82819804bf9dd44a5dffabad06376b4d7542
dkartes <hetyei.kevin@gmail.com>
2022-11-09 19:22:34 +0100

    Maj du token + models + controllers

--- Back/controllers/user.controller.js
+++ Back/controllers/user.controller.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_TOKEN = process.env.JWT_SECRET_TOKEN;
+
+// COURS OC
 
 exports.signUp = (req, res, next) => {
   bcrypt
@@ -32,8 +36,9 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            token: jwt.sign({ userId: user._id }, JWT_SECRET_TOKEN, {
               expiresIn: "24h",
+              message: "Vous etes connecté",
             }),
           });
         })
@@ -41,3 +46,33 @@ exports.login = (req, res, next) => {
     })
     .catch(err => res.status(500).json({ err }));
 };
+
+/* From Scratch 
+
+/*
+
+exports.signUp = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.create({ email, password });
+    res.status(201).json({ message: "Utilisateur crée !" });
+  } catch (err) {
+    res.status(200).send({ err });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+*/
--- Back/models/user.model.js
+++ Back/models/user.model.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
-const bcrypt = require("bcrypt");
 
 //Création du schéma utilisateur
 const userSchema = mongoose.Schema({
@@ -18,6 +17,7 @@ const userSchema = mongoose.Schema({
 
 //on applique le validator au schéma avant d'en faire un model
 userSchema.plugin(uniqueValidator);
+/*
 
 // play function before save into display:"block"
 userSchema.pre("save", async function (next) {
@@ -26,5 +26,20 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+//lecture du models en login
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incrorrect email");
+};*/
+
 /* On export le schéma */
-module.exports = mongoose.model("user", userSchema);
+const UserModel = mongoose.model("user", userSchema);
+module.exports = UserModel;
