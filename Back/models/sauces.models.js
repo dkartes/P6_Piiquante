a153cbeb0fd4c2ebe8ab0c6261df8b7f72a9767f
dkartes <hetyei.kevin@gmail.com>
2022-11-25 14:34:39 +0100

    Création modèle des sauces + début création routes sauce

---
+++ Back/models/sauces.models.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+//Création du schéma sauce
+const sauceSchema = mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  manufacturer: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  mainPepper: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  heat: {
+    type: Number,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  dislikes: {
+    type: Number,
+    default: 0,
+  },
+  usersLiked: {
+    type: ["String<userId>"],
+  },
+  usersDisliked: {
+    type: ["String<userId>"],
+  },
+});
